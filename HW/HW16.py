38f2854cdd20a08e161e447029302bbc81046a17
kspomort <kspomort@gmail.com>
2024-02-25 19:12:46 +0300

    25.02.2024

--- HW/HW16.py
+++ HW/HW16.py
@@ -1,18 +1,33 @@
+# def decor(fn):
+#     def wrap(*args):
+#         return print("Среднее Арифметическое чисел:", ", ".join(map(str, args)), "=", fn(*args) / len(args))
+#
+#     return wrap
+#
+#
+# @decor
+# def func(*args):
+#     summa = sum(args)
+#     print(c, ", ".join(map(str, args)), "=", summa)
+#     return summa
+#
+#
+# func(2, 3, 3, 4)
 
-def decor(fn):
-    def wrap(*args):
-        return print("Среднее Арифметическое чисел:", ", ".join(map(str, args)), "=", fn(*args) / len(args))
+def avg(fn):
+    def wrap(*arg):
+        a = ""
+        for i in arg:
+            a += str(i) + ", "
+        print("Среднее Арифметическое чисел:", a[:-2], "=", fn(*arg))
 
     return wrap
 
 
-@decor
-def func(*args):
-    summa = sum(args)
-    print("Сумма чисел:", ", ".join(map(str, args)), "=", summa)
-    return summa
+@avg
+def summa(*args):
+    print("Сумма чисел:", *args, "=", sum(args))
+    return sum(args)
 
 
-func(2, 3, 3, 4)
-
-
+summa(2, 3, 3, 4)
--- main.py
+++ main.py
@@ -2876,7 +2876,216 @@
 #
 # print("C:\\\\text")
 
-dir_name = 'my_doc'
-file_name = "data.txt"
-print(fr"home\{dir_name}\{file_name}")
-print("home\\" + dir_name + "\\" + file_name)
+# dir_name = 'my_doc'
+# file_name = "data.txt"
+# print(fr"home\{dir_name}\{file_name}")
+# print("home\\" + dir_name + "\\" + file_name)
+
+
+###########################################################################
+# def avg(fn):
+#     def wrap(*arg):
+#         a = ""
+#         for i in arg:
+#             a += str(i) + ", "  # "2, 3, 3, 4, "
+#         print("Среднее арифметическое:", a[:-2], "=", fn(*arg) / len(arg))
+#
+#     return wrap
+#
+#
+# @avg
+# def summa(*args):  # (2, 3, 3, 4)
+#     print("Сумма чисел:", ", ".join(map(str, args)), "=", sum(args))
+#     return sum(args)
+#
+#
+# summa(2, 3, 3, 4)
+###########################################################################
+
+# s = """
+# Несколько
+# строк
+# """
+# # print(s)
+#
+# s1 = '''
+# Несколько
+# строк
+# '''
+# print(s1)
+#
+# "Несколько строк"
+# print(s2)
+
+
+# def square(n):
+#     """Принимает число n, возвращает квадрат числа n."""
+#     print()
+#     return n ** 2
+#
+#
+# print(square(5))
+# print(square.__doc__)
+# # max(5, 5)
+# # len()
+# print(len.__doc__)
+
+
+# from math import pi
+#
+#
+# def cylinder(r, h):
+#     """
+#     Вычисляет площадь цилиндра.
+#
+#     Вычисляет площадь цилиндра на основании заданной высоты и радиуса основания
+#
+#     :param r: положительное число, радиус основания цилиндра
+#     :param h: положительное число, высота цилиндра
+#     :return: положительное число, площадь цилиндра
+#     """
+#     return 2 * pi * r * (r + h)
+#
+#
+# print(cylinder(2, 4))
+# print(cylinder.__doc__)
+# print(max.__doc__)
+# print(dict.__doc__)
+
+# print(ord('a'))
+# print(ord('#'))
+# print(ord('й'))
+
+# while True:
+#     n = input("-> ")
+#     if n != "-1":
+#         print(ord(n))
+#     else:
+#         break
+
+# s = "Test string for me"
+# arr = [ord(x) for x in s]
+# print("ASCII коды:", arr)
+# arr = [int(sum(arr) / len(arr))] + arr
+# print("Среднее арифметическое:", arr)
+# arr += [ord(x) for x in input("-> ")[:3] if ord(x) not in arr]
+# print(arr)
+# print(arr.count(arr[-1]) - 1)
+# arr.sort(reverse=True)
+# print(arr)
+
+
+# print(chr(97))
+# print(chr(8364))
+
+# a = 97
+# b = 122
+#
+# if a < b:
+#     a, b = b, a
+#
+# for i in range(b, a + 1):
+#     print(chr(i), end=" ")
+
+# print("apple" == "Apple")
+# print("apple" > "Apple")  # 97 > 65
+
+
+# from random import randint
+#
+# shortest = 7
+# longest = 16
+# min_ascii = 33
+# max_ascii = 126
+#
+#
+# def random_password():
+#     res = ""
+#     for i in range(randint(shortest, longest)):  # range(10)
+#         rand_char = chr(randint(min_ascii, max_ascii))
+#         res += rand_char
+#     return res
+#
+#
+# print("Ваш случайный пароль:", random_password())
+
+
+# s = "hello, WORLD! I am learning Python."
+# # print(s.capitalize())  # Hello, world! i am learning python.
+# # print(s.lower())  # hello, world! i am learning python.
+# # print(s.upper())  # HELLO, WORLD! I AM LEARNING PYTHON.
+# # # print(s.swapcase())  # HELLO, world! i AM LEARNING pYTHON.
+# # # print(s.title())  # Hello, World! I Am Learning Python.
+# # print(s)
+#
+# # print(s.count("l"))
+# # print(s.count("l", 3))
+# # print(s.count("l", 3, 10))
+#
+# # print(s.find("Python1"))  # возвращает индекс первого вхождения подстроки в строку, если такой подстроки нет
+# # # возвращает "-1"
+# # # print(s.find("l", 4, 20))
+# # print(s.find("l"))
+# # print(s.rfind("l"))
+# #
+# # print(s.index("l"))  # ValueError
+# # print(s.rindex("l"))
+#
+#
+# # st = input("Введите два слова через пробел: ")
+# # first = st[:st.find(" ")]
+# # second = st[st.find(" ") + 1:]
+# # print(second + " " + first)
+# print(s.startswith("I am", 14))
+# print(s.index("I am"))
+# print(s.endswith("on."))
+
+
+# print(int("789"))
+
+# print('123'.isdigit())  # только числа
+# print('qЙwee'.isalpha())  # только буквы
+#
+# print('Abc123'.isalnum())  # только буквы или цифры
+#
+# print('aИc0123"№'.islower())  # только нижний регистр
+# print('ASD0123"№'.isupper())  # только верхний регистр
+
+# n = input("Введите число: ")
+# if n.isdigit():
+#     n = int(n)
+#     print(n * 2)
+
+
+# print('py'.center(10))
+# print(' py '.center(11, "-"))
+
+
+# print('    py    '.lstrip())
+# print('    py    '.rstrip())
+# print('    p y    '.strip())
+#
+# print('https://www.pythons.org'.strip('/:pths.org'))
+# print('https://www.pythons.org'.lstrip('/:pths').rstrip('.org'))
+
+
+# str1 = "Я изучаю Nython. Мне нравится Nython. Nython очень интересный язык программирования. New"
+# print(str1.replace("Nython", "Python"))
+# print(str1.replace("N", "P"))
+
+
+# s = "-"
+# seq = ("a", "b", "c")
+# print(s.join(seq))
+#
+# print("..".join(['1', '99']))
+#
+# print(", ".join("Hello"))
+
+# print("Строка разделенная пробелами".split())  # ['Строка', 'разделенная', 'пробелами']
+# print('www.python.org.ru'.split(".", 2))
+# print('www.python.org.ru'.rsplit(".", 2))
+
+a = input("-> ").split()
+b = list(map(int, a))
+print(b)
