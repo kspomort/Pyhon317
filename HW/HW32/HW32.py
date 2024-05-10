3172103b16e929a321fcf7b608f06ceac4aa2174
kspomort <kspomort@gmail.com>
2024-05-10 13:01:48 +0300

    20.04.2024

--- HW/HW32/HW32.py
+++ HW/HW32/HW32.py
@@ -1,45 +1,43 @@
+import requests
 import json
-from random import choice
+
+response = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos = json.loads(response.text)
+
+todos_by_user = {}  # {1: 10, }
+
+for todo in todos:
+    if todo["completed"]:
+        try:
+            todos_by_user[todo['userId']] += 1
+        except KeyError:
+            todos_by_user[todo['userId']] = 1
+print(todos_by_user)
+
+top_users = sorted(todos_by_user.items(), key=lambda x: x[1], reverse=True)
+print(top_users)
+
+max_complete = top_users[0][1]
+print(max_complete)
+
+users = []
+for user, num_complete in top_users:
+    if num_complete < max_complete:
+        break
+    users.append(str(user))
+print(users)
+
+max_users = " and ".join(users)
+print(max_users)
+print(f"Users {max_users} completed {max_complete} TODOS")
 
 
-def gen_person():
-    ids = ''
-    name = ''
-    tel = ''
-
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-    while len(name) != 7:
-        name += choice(letters)
-
-    while len(tel) != 10:
-        tel += choice(nums)
-
-    while len(ids) != 15:
-        ids += choice(nums)
-
-    person = {
-        ids: {
-            'name': name,
-            'tel': tel
-            }
-    }
-
-    return person
+def keep(todo):
+    completed = todo["completed"]
+    max_count = str(todo["userId"]) in users
+    return completed and max_count
 
 
-def write_json(person_list):
-    try:
-        data = json.load(open('persons.json'))
-    except FileNotFoundError:
-        data = {}
-
-    data.update(person_list)
-
-    with open("persons.json", 'w') as f:
-        json.dump(data, f, indent=2)
-
-
-for i in range(5):
-    write_json(gen_person())
+with open("filter_file.json", "w") as f:
+    filtered = list(filter(keep, todos))
+    json.dump(filtered, f, indent=2)
